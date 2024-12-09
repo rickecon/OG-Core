3ec464aaa96b189c6c707391a262f146775a5829
Richard Evans <rickecon@gmail.com>
2024-12-09 17:31:40 -0500

    Updated txfunc.py

--- ogcore/txfunc.py
+++ ogcore/txfunc.py
@@ -112,7 +112,7 @@ def get_tax_rates(
             np.squeeze(params[..., 1]),
         )
         if rate_type == "etr":
-            flat_pct_incr_base = 0.03
+            flat_pct_incr_base = 0.045
             txrates = (
                 1 - (lambda_s * (income ** (-tau_s))) + flat_pct_incr_base
             )
