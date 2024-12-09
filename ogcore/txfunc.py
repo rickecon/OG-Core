25e0c0a50bbdb56403ef262e3db1607456fa0f44
Richard Evans <rickecon@gmail.com>
2024-12-09 16:38:31 -0500

    Updated txfunc.py

--- ogcore/txfunc.py
+++ ogcore/txfunc.py
@@ -112,7 +112,10 @@ def get_tax_rates(
             np.squeeze(params[..., 1]),
         )
         if rate_type == "etr":
-            txrates = 1 - (lambda_s * (income ** (-tau_s)))
+            flat_pct_incr_base = 0.03
+            txrates = (
+                1 - (lambda_s * (income ** (-tau_s))) + flat_pct_incr_base
+            )
         else:  # marginal tax rate function
             txrates = 1 - (lambda_s * (1 - tau_s) * (income ** (-tau_s)))
     elif tax_func_type == "DEP":
